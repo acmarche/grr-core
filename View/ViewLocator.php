73b44860baaa42fe06aca7fc7947f68543c83f84
jfsenechal <jfsenechal@gmail.com>
2020-10-07 21:08:27 +0200

    move templates and class

---
+++ View/ViewLocator.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Grr\Core\View;
+
+use Grr\Core\Contrat\Front\ViewInterface;
+use Symfony\Component\DependencyInjection\ServiceLocator;
+
+class ViewLocator
+{
+    /**
+     * @var \Traversable
+     */
+    private $renders;
+    /**
+     * @var ServiceLocator
+     */
+    private $serviceLocator;
+
+    public function __construct(
+        \Traversable $renders,
+        ServiceLocator $serviceLocator
+    ) {
+        $this->renders = $renders;
+        $this->serviceLocator = $serviceLocator;
+    }
+
+    public function loadAllInterface(): \Traversable
+    {
+        return $this->renders;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function findViewerByView(string $key): ViewInterface
+    {
+        if ($this->serviceLocator->get($key)) {
+            return $this->serviceLocator->get($key);
+        }
+        throw new \Exception('No class found for this vue '.$key);
+    }
+}
