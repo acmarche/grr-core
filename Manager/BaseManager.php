b0a64042445469053db305f8d94bafebe6ddcede
jfsenechal <jfsenechal@gmail.com>
2020-10-07 21:17:00 +0200

    move

---
+++ Manager/BaseManager.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of GrrSf application.
+ *
+ * @author jfsenechal <jfsenechal@gmail.com>
+ * @date 23/09/19
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Grr\Core\Manager;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+abstract class BaseManager
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getEntityManager(): EntityManagerInterface
+    {
+        return $this->entityManager;
+    }
+
+    public function persist(object $object): void
+    {
+        $this->entityManager->persist($object);
+    }
+
+    public function remove(object $object): void
+    {
+        $this->entityManager->remove($object);
+    }
+
+    public function flush(): void
+    {
+        $this->entityManager->flush();
+    }
+
+    public function insert(object $object): void
+    {
+        $this->entityManager->persist($object);
+        $this->flush();
+    }
+}
---
+++ Manager/ManagerInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jfsenechal
+ * Date: 1/03/19
+ * Time: 20:08.
+ */
+
+namespace Grr\Core\Manager;
+
+interface ManagerInterface
+{
+    public function persist();
+
+    public function remove();
+
+    public function flush();
+
+    public function insert();
+}
