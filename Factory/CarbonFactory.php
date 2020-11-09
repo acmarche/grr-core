030031f72a66b1db9f3ed3a3296b32f5e45002f4
jfsenechal <jfsenechal@gmail.com>
2020-11-09 15:06:18 +0100

    fix locale date

--- Factory/CarbonFactory.php
+++ Factory/CarbonFactory.php
@@ -3,6 +3,7 @@
 namespace Grr\Core\Factory;
 
 use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 use Carbon\CarbonInterface;
 use DateTimeInterface;
 use Grr\Core\I18n\LocalHelper;
@@ -27,6 +28,13 @@ class CarbonFactory
     public function instance(DateTimeInterface $date): CarbonInterface
     {
         return $this->setLocale(Carbon::instance($date));
+    }
+
+    public function instanceImmutable(DateTimeInterface $date): CarbonImmutable
+    {
+        $dateCreated = $this->setLocale(Carbon::instance($date));
+
+        return $dateCreated->toImmutable();
     }
 
     public function setLocale(CarbonInterface $date): CarbonInterface
