b139ed8b2f08128fd81557e65f7df2224b021064
jfsenechal <jfsenechal@gmail.com>
2024-02-09 11:21:50 +0100

    fix

--- Doctrine/TablePrefix.php
+++ Doctrine/TablePrefix.php
@@ -2,54 +2,35 @@
 
 namespace Grr\Core\Doctrine;
 
-use Doctrine\Common\EventSubscriber;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
-use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
-/**
- * Pour l'activer dans config/services.yaml :
- * Grr\Core\Doctrine\TablePrefix:
- *  arguments:
- *    $prefix: '%env(string:DATABASE_PREFIX)%'
- *  tags:
- *    - { name: doctrine.event_subscriber, connection: default }.
- *
- * Class TablePrefix
- */
-class TablePrefix implements EventSubscriber
+#[AsDoctrineListener('loadClassMetadata')]
+class TablePrefix
 {
     public function __construct(
-        protected string $prefix = 'grr_'
+        #[Autowire('%env(string:DATABASE_PREFIX)%')]
+        protected ?string $prefix = null
     ) {
     }
 
-    /**
-     * Returns an array of events this subscriber wants to listen to.
-     *
-     * @return string[]
-     */
-    public function getSubscribedEvents(): array
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
     {
-        return ['loadClassMetadata'];
-    }
-
-    public function loadClassMetadata(LoadClassMetadataEventArgs $loadClassMetadataEventArgs): void
-    {
-        $classMetadata = $loadClassMetadataEventArgs->getClassMetadata();
-
-        if (! $classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName(
-            ) === $classMetadata->rootEntityName) {
-            $classMetadata->setPrimaryTable(
-                [
+        if ($this->prefix) {
+            $classMetadata = $eventArgs->getClassMetadata();
+            if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName(
+                ) === $classMetadata->rootEntityName) {
+                $classMetadata->setPrimaryTable([
                     'name' => $this->prefix.$classMetadata->getTableName(),
-                ]
-            );
-        }
-
-        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
-            if (ClassMetadataInfo::MANY_TO_MANY == $mapping['type'] && $mapping['isOwningSide']) {
-                $mappedTableName = $mapping['joinTable']['name'];
-                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix.$mappedTableName;
+                ]);
+            }
+            foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+                if ($mapping['type'] == ClassMetadata::MANY_TO_MANY && $mapping['isOwningSide']) {
+                    $mappedTableName = $mapping['joinTable']['name'];
+                    $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix.$mappedTableName;
+                }
             }
         }
     }
